22b93bc8d1d92e5f065722e0ce922fa8ca82529f
DorregoBraian <rayan_22_dorrego@hotmail.com>
2023-10-28 01:49:50 -0300

    cofiguracion GuitHub Page 2

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/DorregoBraian/Peliculas-Web-Vue.git/'
+    ? '/Peliculas-Web-Vue/'
     : '/'
 }
